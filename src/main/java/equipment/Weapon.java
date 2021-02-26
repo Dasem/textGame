2825dce8ed64f14d6b361e93ddb4d4c60a846f38
Rigt <andr614@yandex.ru>
2021-02-27 02:03:15 +0500

    Взаимодействие с оружием

--- src/main/java/equipment/Weapon.java
+++ src/main/java/equipment/Weapon.java
@@ -29,22 +29,40 @@ public class Weapon extends Equipment {
     public void execute() {
         weaponMenu().showAndChoose();
     }
-    private Menu weaponMenu(){
+
+    private Menu weaponMenu() {
         Menu weaponMenu = new Menu("Меню для оружия");
-        weaponMenu.addItem("Использовать это оружие", () -> {
-            this.equipWeapon();
-            System.out.println("Ну теперь точно всем вломлю");
-            Character.getInstance().getInventory().removeItem(this);
-        });
+        if (this == Character.getInstance().getWeapon()) {
+            weaponMenu.addItem("Убрать оружие", () -> {
+                this.removeWeapon();
+                System.out.println("Пускай полежит пока");
+                Character.getInstance().getInventory().addItem(this);
+            });
+
+        } else {
+            weaponMenu.addItem("Использовать это оружие", () -> {
+                this.equipWeapon();
+                System.out.println("Ну теперь точно всем вломлю");
+                Character.getInstance().getInventory().removeItem(this);
+            });
+        }
         weaponMenu.addItem("Оставить как есть", () -> System.out.println("Моя пушка получше будет"));
         return weaponMenu;
     }
 
-    public void equipWeapon(){
+    public void equipWeapon() {
         Character.getInstance().setWeapon(this);
 
     }
 
+    public void removeWeapon() {
+        Character.getInstance().setWeapon(null);
 
+    }
+
+    public String getPrettyName() {
+        return "Оружие: "+getWeaponType().getTitle()+"\nМакс урон: "+getWeaponType().getDamage();
+
+    }
 
 }
