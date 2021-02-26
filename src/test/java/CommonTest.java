65f32cf77fda407005bf6fb5e4dc3753ae4156fa
Тюй <povaristi@gmail.com>
2021-02-26 16:35:37 +0500

    пробовал цыиклы ,массивы. Ввод с клавиатуры.

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -1,8 +1,29 @@
 import mechanic.labyrinth.*;
 import units.Character;
 
+import java.util.Scanner;
+
 public class CommonTest {
+    public static int[] massivIntov() {
+        Scanner ara = new Scanner(System.in);
+        int n = ara.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = ara.nextInt();
+        }
+        return arr;
+    }
+
+    public static void vivodFunc(int[] hzmass) {
+        for (int i = 0; i < hzmass.length; i++) {
+            System.out.println(hzmass[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
-        //new Labyrinth().enterLabyrinth(new Character("123"));
+        int[] massInt = massivIntov();
+        vivodFunc(massInt);
+
     }
 }
